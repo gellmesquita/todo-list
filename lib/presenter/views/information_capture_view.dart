640cc7babaa142b5bed215f6ca3ac1f8d25f8012
Gelson Mesquita <88181879+gellmesquita@users.noreply.github.com>
2023-11-16 12:59:58 +0100

    feat: buscando as actividades do preferences

--- lib/presenter/views/information_capture_view.dart
+++ lib/presenter/views/information_capture_view.dart
@@ -30,8 +30,6 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,11 +78,10 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
               Observer(
                 builder: (context) {
                   var activity = widget.controller.activities;
-                  return  ListView.builder(
+                  return ListView.builder(
                     itemCount: activity.length,
-                    itemBuilder: (context, index) => itemActivity(
-                      activity[index]
-                    ),
+                    itemBuilder: (context, index) =>
+                        itemActivity(activity[index]),
                   );
                 },
               )
@@ -97,51 +94,52 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
 
   Widget itemActivity(ActivitiesEntity activity) {
     return Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: DP16,
-              ),
-              decoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(color: gray400))),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      activity.description,
-                      style: TextStyle(color: gray800),
-                    ),
+      padding: const EdgeInsets.symmetric(
+        vertical: DP16,
+      ),
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: gray400))),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              activity.description,
+              style: TextStyle(color: gray800),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: DP10),
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    editActivityDialog(activity, widget.controller.editLoading);
+                  },
+                  child: const Icon(
+                    Icons.edit,
+                    color: gray400,
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: DP10),
-                    alignment: Alignment.center,
-                    child: Row(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            editActivityDialog(activity, widget.controller.editLoading);
-                          },
-                          child:const Icon(
-                            Icons.edit,
-                            color: gray400,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: DP2,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            deleteActivityDialog(activity.id, widget.controller.deleteLoading);
-                          },
-                          child: const Icon(
-                            Icons.delete_outline_outlined,
-                            color: primary400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            );
+                ),
+                const SizedBox(
+                  width: DP2,
+                ),
+                InkWell(
+                  onTap: () {
+                    deleteActivityDialog(
+                        activity.id, widget.controller.deleteLoading);
+                  },
+                  child: const Icon(
+                    Icons.delete_outline_outlined,
+                    color: primary400,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   void _checkIfTextFilled() {
@@ -172,15 +170,15 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
             padding: EdgeInsets.symmetric(horizontal: DP30, vertical: DP30),
             child: Column(
               children: [
-                Text(
+                const Text(
                   "Deseja eliminar essa atividade ?",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: DP16, fontWeight: FontWeight.w700),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: DP12,
                 ),
-                Text(
+                const Text(
                   "Ao eliminar essa atividade não terá como reverter essa acção, toda informação será perdida!",
                   style: TextStyle(color: gray500),
                 ),
@@ -191,9 +189,11 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
                     InkWell(
                       onTap: widget.controller.goBack,
                       child: Container(
-                        padding: EdgeInsets.symmetric(
-                            vertical: DP10, horizontal: DP10),
-                        child: Text(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: DP10,
+                             horizontal: DP10
+                        ),
+                        child: const Text(
                           "Cancelar",
                           style: TextStyle(
                               fontSize: DP14,
@@ -252,16 +252,12 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
             padding: EdgeInsets.symmetric(horizontal: DP30, vertical: DP30),
             child: Column(
               children: [
-               
                 InputTextField(
-                  onChanged: (p0) {}, 
+                  onChanged: (p0) {},
                   controller: textController,
                   focus: focus,
-                  onSubmit: (p0) {
-                    
-                  },
+                  onSubmit: (p0) {},
                 ),
-                 
                 Expanded(child: Container()),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -270,16 +266,13 @@ class _InformationCaptureViewState extends State<InformationCaptureView> {
                       onTap: widget.controller.goBack,
                       child: Container(
                         padding: const EdgeInsets.symmetric(
-                            vertical: DP10, 
-                            horizontal: DP10
-                        ),
+                            vertical: DP10, horizontal: DP10),
                         child: const Text(
                           "Cancelar",
                           style: TextStyle(
                               fontSize: DP14,
                               fontWeight: FontWeight.bold,
-                              color: gray800
-                          ),
+                              color: gray800),
                         ),
                       ),
                     ),
